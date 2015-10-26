af5c7b534693a3dbcbeb6bb798431e22508c9cf5
NT <nickt444@gmail.com>
2015-10-26 14:11:26 -0400

    Each item now has a link to a functioning review state with a list of all reviews

--- browser/js/reviews/reviews.controller.js
+++ browser/js/reviews/reviews.controller.js
@@ -1,3 +1,13 @@
 /**
  * Created by uzer-y on 10/24/15.
  */
+
+app.controller('reviewsListCtrl', function($scope, ProductList, ReviewsFactory, product ) {
+    $scope.product = product;
+    //$scope.product = function(id) {
+    //    return ProductList.getOne(id)
+    //};
+
+    $scope.reviews = ReviewsFactory.getAll($scope.product._id);
+
+});
--- browser/js/reviews/reviews.directive.js
+++ browser/js/reviews/reviews.directive.js
@@ -1,3 +1,13 @@
 /**
  * Created by uzer-y on 10/24/15.
  */
+
+app.directive('reviewList', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            review: '='
+        },
+        templateUrl: '/js/reviews/reviews.directive.html'
+    };
+});
