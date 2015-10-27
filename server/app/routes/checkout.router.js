e92db5eda5759f05f327ea54a403ebfeebd4e088
IanAllen <ianavilallen@gmail.com>
2015-10-27 15:02:36 -0400

    Added scaffold to backend checkout route

--- server/app/routes/checkout.router.js
+++ server/app/routes/checkout.router.js
@@ -1,14 +1,44 @@
-var Order = mongoose.model('Order');
-var stripe = require('stripe');
+var Order = require('mongoose').model('Order');
+var stripeKey = require('../../../config').STRIPE.apiKey;
+var stripe = require('stripe')(stripeKey);
 var router = require('express').Router()
 
 
-router.post('/:orderId', function(req, res, next) {
-  Order.findById(req.params.orderId)
+router.get('/:id', function(req, res, next) {
+  Order.findPopulatedOrder(req.params.id)
   .then(function(order) {
-    res.json(order)
+
+    // Add logic to store or retrieve saved tokens for users.
+
+    makeCharge(order, req.body.stripeToken).then(function(result) {
+      res.json(order, result);
+      
+      /* 
+      store 'safe' card info from API response
+      and relevant details, such as amount, expiration year, last 4 digits
+      and billing address
+      */
+
+    })
+    .then(null, function(err) {
+      if (err.type === 'StripeCardError') {
+        res.status(404).json(err.message)
+      } else {
+        res.status(400).json(err)
+      }
+    })
+
   })
+  .then(null, next);
 })
 
 
 module.exports = router;
+
+function makeCharge(order, userToken) {
+  return stripe.charges.create({
+    source: userToken,
+    amount: order.totalPrice,
+    description: order._id
+  })
+}
