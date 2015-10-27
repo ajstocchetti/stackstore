9641d37dcaa7a28aa263826d80d0bf6dc2b79246
IanAllen <ianavilallen@gmail.com>
2015-10-27 00:09:18 -0400

    added stripe and started checkout route

--- server/app/routes/access.middleware.js
+++ server/app/routes/access.middleware.js
@@ -23,7 +23,7 @@ module.exports = {
   },
 
   // Only admins can get past this
-  hasAdminRights: function(req, res, next) {
+  isAdmin: function(req, res, next) {
     if (req.user.isAdmin) {
       next()
     } else {
