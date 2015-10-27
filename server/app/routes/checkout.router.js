071579355f1eae7ff7c14f315bf743cec41a0b29
IanAllen <ianavilallen@gmail.com>
2015-10-27 08:00:23 -0400

    created new seed file

--- server/app/routes/checkout.router.js
+++ server/app/routes/checkout.router.js
@@ -3,5 +3,12 @@ var stripe = require('stripe');
 var router = require('express').Router()
 
 
+router.post('/:orderId', function(req, res, next) {
+  Order.findById(req.params.orderId)
+  .then(function(order) {
+    res.json(order)
+  })
+})
+
 
 module.exports = router;
