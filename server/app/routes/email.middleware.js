e34127ce51dafcbd6d87b2142582a87a09220b5a
Matt Longoria <mattlongoria@gmail.com>
2015-10-29 18:19:15 -0400

    switched hard coded testing email to team gmail

--- server/app/routes/email.middleware.js
+++ server/app/routes/email.middleware.js
@@ -23,7 +23,7 @@ module.exports = {
 		// setup e-mail data with unicode symbols
 		var mailOptions = {
 		    from: 'Order Droid <team.brotein.stackstore@gmail.com>', // sender address
-		    to: 'mattlongoria@gmail.com', // list of receivers
+		    to: 'team.broetin.stackstore@gmail.com', // list of receivers
 		    subject: 'Your order is on the way!', // Subject line
 		    text: 'the plain text', // plaintext body
 		    html: output // html body
