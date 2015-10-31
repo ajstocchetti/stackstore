fea8806cdccc0f561d8094e5553a395895b9a6ff
IanAllen <ianavilallen@gmail.com>
2015-10-30 21:44:43 -0400

    cleaned up submission and testing order details

--- server/app/routes/checkout.router.js
+++ server/app/routes/checkout.router.js
@@ -1,34 +1,34 @@
 var Order = require('mongoose').model('Order');
 var stripeKey = require('../../../config').STRIPE.apiKey;
 var stripe = require('stripe')(stripeKey);
-var router = require('express').Router()
+var router = require('express').Router();
+var _ = require('lodash');
 
 router.post('/:id', function(req, res, next) {
-  // console.log(req.body.id);
+  console.log(req.body);
   
   Order.findOne(req.params.id)
   .then(function(order) {
     // console.log(order)
-    checkout(order, req.body.id).then(function(result) {
-       console.log("hello?")
-      res.json(result);
-     
-      /*
-      store 'safe' card info from API response
-      and relevant details, such as amount, expiration year, last 4 digits
-      and billing address
-      */
+    var o = savePaymentDetails(order, req)
+    console.log(o)
+    // checkout(order, req.body.id).then(function(result) {
+      
+    //   res.json(result);
+     res.json(o)
+ 
 
     })
-    .then(null, function(err) {
-      if (err.type === 'StripeCardError') {
-        res.status(404).json(err.message)
-      } else {
-        console.log(err, err.type, err.message)
-        res.status(400).json(err.message)
-      }
-    })
-  })
+    // .then(null, function(err) {
+    //   if (err.type === 'StripeCardError') {
+    //     console.log(err.message)
+    //     res.status(404).json(err.message)
+    //   } else {
+    //     console.log(err, err.type, err.message)
+    //     res.status(400).json(err.message)
+    //   }
+    // })
+  // })
 })
 
 module.exports = router;
@@ -37,8 +37,17 @@ function checkout(order, userToken) {
   console.log('in checkout')
   return stripe.charges.create({
     source: userToken,
-    amount: 3000,
+    amount: order.total,
     currency: "usd",
     description: order.toString()._id
   })
+}
+
+function savePaymentDetails(order, req) {
+  order.shipping.address.push(req.body.shippingAddress);
+  order.billing.address.push(req.body.billingAddress);
+  order.billing.last4 = req.body.last4;
+  order.billing.expMonth = req.body.expMonth;
+  order.billing.expYear = req.body.expYear;
+  return order
 }
