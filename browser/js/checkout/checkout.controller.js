6685c9998e02bd9262fc2abc7b6c924875941d1e
IanAllen <ianavilallen@gmail.com>
2015-10-30 21:44:43 -0400

    cleaned up submission and testing order details

--- browser/js/checkout/checkout.controller.js
+++ browser/js/checkout/checkout.controller.js
@@ -1,6 +1,8 @@
 app.controller('checkoutCtrl', function($scope, order, step, AuthService, PaymentFactory) {
   $scope.order = order;
   $scope.step = step;
+  $scope.login = {};
+  $scope.error = null;
   $scope.shippingAddress = {};
   $scope.billingAddress = {};
 
@@ -8,8 +10,19 @@ app.controller('checkoutCtrl', function($scope, order, step, AuthService, Paymen
     return AuthService.isAuthenticated();
   }
 
+  $scope.sendLogin = function(credentials) {
+    AuthService.login(credentials)
+    .then(function(result) {
+      $scope.step = 'address';
+    })
+    .catch(function(err) {
+      $scope.error = 'Invalid login credentials.';
+    })
+  }
+
   $scope.confirmOrder = function() {
     $scope.step = "shipping";
+    $scope.error = null;
   }
 
   $scope.copyShipping = function() {
@@ -18,14 +31,26 @@ app.controller('checkoutCtrl', function($scope, order, step, AuthService, Paymen
 
   $scope.confirmAddress = function() {
     $scope.step = "payment"
+    $scope.error = null;
   }
 
   $scope.stripeCallback = function (data, result) {
     if (result.error) {
+      $scope.error = result.error.message;
       window.alert('it failed! error: ' + result.error.message);
     } else {
       window.alert('success! token: ' + result.id);
-      PaymentFactory.checkout(order._id, result)
+
+      var billing = {
+        billingAddress: $scope.billingAddress,
+        shippingAddress: $scope.shippingAddress,
+        stripeToken: result.id,
+        last4: result.card.last4,
+        expMonth: result.card.exp_month,
+        expYear: result.card.exp_year
+      }
+
+      PaymentFactory.checkout(order._id, billing);
 
     }
     console.log(result)
