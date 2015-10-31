608a173666cb04dd6076fe1fe99bcde45c5c7625
tackypinkflamingos <ajstocchetti@gmail.com>
2015-10-31 09:22:51 -0400

    WIP cancel button :(

--- browser/js/accountmgmt/cancelorder.directive.js
+++ browser/js/accountmgmt/cancelorder.directive.js
@@ -2,11 +2,19 @@ app.directive('cancelOrder', ['OrderFactory', function () {
 	return {
 		restrict: 'E',
 		templateUrl: '/js/accountmgmt/cancelorder.directive.html',
-		// scope: {
-		// 	order: '='
-		// },
+		scope: {
+			order: '='
+			// zcancel: OrderFactory.cancelOrder
+		},
 		link: function(scope, element, attr) {
-			scope.cancel = OrderFactory.cancelOrder
+			// scope.cancel = OrderFactory.cancelOrder
+			scope.cancel = function(id) {
+				alert(id)
+				OrderFactory.cancelOrder;
+			};
+			scope.zcancel = function() {
+				OrderFactory.cancelOrder;
+			}
 		}
 	}
 }]);
