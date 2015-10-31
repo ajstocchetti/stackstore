bdfd2cfce7372c280f57a6ff1fc0b7b0330c4a68
tackypinkflamingos <ajstocchetti@gmail.com>
2015-10-31 14:40:44 -0400

    basic leave review functionality

--- browser/js/reviews/edit/review.edit.state.js
+++ browser/js/reviews/edit/review.edit.state.js
@@ -4,12 +4,34 @@ app.config(function($stateProvider) {
     url: '/:id/reviews/edit',
     templateUrl: 'js/reviews/edit/review.edit.html',
     resolve: {
-        product: function(ProductList, $stateParams ) {
-            return ProductList.getOne($stateParams.id)
-        }
+      product: function(ProductList, $stateParams) {
+        return ProductList.getOne($stateParams.id);
+      },
+      me: function(AccountFactory) {
+        return AccountFactory.me();
+      }
     },
-    controller: function($scope, product) {
+    controller: function($scope, product, ReviewsFactory, me, $state) {
       $scope.product = product;
+      $scope.userReview = {
+        author: me._id,
+        ratings: 3
+      };
+      $scope.save = function(review) {
+        review.stars = review.ratings;
+        return ReviewsFactory.create(product._id, review)
+        .then(function() {
+          $state.go("productDetail", { id: product._id })
+        })
+      };
+      $scope.ratingUp = function() {
+        if($scope.userReview.ratings < 5)
+          $scope.userReview.ratings++;
+      };
+      $scope.ratingDown = function() {
+        if($scope.userReview.ratings > 0)
+          $scope.userReview.ratings--;
+      };
     }
-  })
-})
+  });
+});
--- browser/js/reviews/reviews.factory.js
+++ browser/js/reviews/reviews.factory.js
@@ -7,7 +7,8 @@
 app.factory('ReviewsFactory', function ($http) {
     return {
         getAll: getAll,
-        getOne: getOne
+        getOne: getOne,
+        create: create
     };
 
     function getAll(productId) {
@@ -33,7 +34,9 @@ app.factory('ReviewsFactory', function ($http) {
             date: Date.now()
         };
 
-        return $http.post('/api/products/reviews/' + productID, config )
+        console.log(config);
+
+        return $http.post('/api/products/'+productID+'/reviews/', config )
             .then(function (results) {
                 return results.data;
             });
